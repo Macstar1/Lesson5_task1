318d5dac9dda5b5d0dc7b41780e313a90b9a922f
maopa <maopa@bk.ru>
2024-10-04 16:00:29 +0300

    Attachment array added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,27 @@ fun main() {
     val like: Likes = Likes()
     val post: Post = Post()
 
+    post.attachment = arrayOf(
+        AttachmentFile(
+            File(
+                1,
+                1,
+                "",
+                10,
+                "url"
+            ),
+            "file"
+        ),
+        AttachmentAudio(Audio(
+            1L,
+            1L,
+            "Duke Dumont",
+            "Ocean Drive",
+            5
+        ), "audio")
+    )
+
+
 
     println(post)
 
